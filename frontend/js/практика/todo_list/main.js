a6fa2f192ed02fa62f5f1af8ec76b5f9f4df41c4
Luffy <146634615+ZhaminZhalilov@users.noreply.github.com>
2024-05-09 13:20:19 +0300

    creating a head

--- frontend/js/практика/todo_list/main.js
+++ frontend/js/практика/todo_list/main.js
@@ -1,12 +1,40 @@
-let taskArrays = [];
+let  taskArrays = [];
 
 const button = document.querySelector('.buttonAdd')
-button.addEventListener('click',addTask);
+button.addEventListener('click',submitButtonClicked);
 
 
-function addTask(){
-    const inputValue = document.querySelector('textField').value;
-    taskArrays.push(inputValue);
-    console.log(taskArrays)
+function submitButtonClicked(){
+    const inputValue = document.querySelector('.textField');
+    taskArrays.push(inputValue.value);
+    taskArrays.forEach((task) => {
+        console.log(task)
+    })
 }
-console.log(button)
+
+function renderTask(task){
+    const header = querySelector('.header')
+
+    const div = createElement('div')
+    div.className += 'Tasks'
+
+    const spanTask = querySelector('span')
+    spanTask.className += 'task'
+
+    div.appendChild(spanTask)
+    header.afterChild(div)
+
+
+}
+const header = document.querySelector('.header')
+
+const div = document.createElement('div')
+div.className += 'Tasks'
+
+const spanTask = document.createElement('span')
+spanTask.textContent = 'sdssd'
+spanTask.classList.add('textField, task')
+
+
+div.append(spanTask)
+header.after(div)
