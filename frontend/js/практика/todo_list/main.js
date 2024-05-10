95438ddf89bf7cd378aaa10787097618c3d7cbcf
Luffy <146634615+ZhaminZhalilov@users.noreply.github.com>
2024-05-10 10:44:59 +0300

    buttons to delete
    adding buttons to delete tasks

--- frontend/js/практика/todo_list/main.js
+++ frontend/js/практика/todo_list/main.js
@@ -1,40 +1,52 @@
 let  taskArrays = [];
 
-const button = document.querySelector('.buttonAdd')
+const button = document.querySelector('.buttonAdd');
 button.addEventListener('click',submitButtonClicked);
 
 
 function submitButtonClicked(){
     const inputValue = document.querySelector('.textField');
     taskArrays.push(inputValue.value);
-    taskArrays.forEach((task) => {
-        console.log(task)
-    })
+
+    renderTask(inputValue.value);
+    inputValue.value = '';
 }
 
-function renderTask(task){
-    const header = querySelector('.header')
-
-    const div = createElement('div')
-    div.className += 'Tasks'
-
-    const spanTask = querySelector('span')
-    spanTask.className += 'task'
-
-    div.appendChild(spanTask)
-    header.afterChild(div)
+function renderTask(textValue){
+    const header = document.querySelector('.header');
+    const tasks = document.querySelector('.Tasks');
+    
+    
 
 
+    if (taskArrays.length===1){
+        
+        const spanTask = document.createElement('span');
+        spanTask.classList.add('task');
+        spanTask.textContent = textValue;
+
+        const spanGarbage = document.createElement('span')
+        spanGarbage.classList.add('garbage')
+        spanGarbage.textContent = 'мусор'
+
+        const container =  document.createElement('div');
+        container.classList.add('container')
+
+        
+        header.after(tasks);
+        tasks.append(container);
+        container.append(spanTask)
+        container.append(spanGarbage)
+
+    }
+    else{
+        const spanTask = document.createElement('span');
+        spanTask.classList.add('task');
+        spanTask.textContent = textValue;
+        
+        tasks.append(spanTask);
+    }
+   console.log(taskArrays);
 }
-const header = document.querySelector('.header')
-
-const div = document.createElement('div')
-div.className += 'Tasks'
-
-const spanTask = document.createElement('span')
-spanTask.textContent = 'sdssd'
-spanTask.classList.add('textField, task')
 
 
-div.append(spanTask)
-header.after(div)
