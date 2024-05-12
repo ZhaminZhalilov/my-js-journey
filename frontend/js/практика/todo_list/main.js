4e93116ecce7159ef2ad090e4e3a24bd8dee8515
Luffy <146634615+ZhaminZhalilov@users.noreply.github.com>
2024-05-12 07:04:45 +0300

    :)

--- frontend/js/практика/todo_list/main.js
+++ frontend/js/практика/todo_list/main.js
@@ -3,17 +3,33 @@ let  taskArrays = [];
 const button = document.querySelector('.buttonAdd');
 button.addEventListener('click',submitButtonClicked);
 
-const buttonDel = document.querySelectorAll('.garbage');
-buttonDel.addEventListener('click',deleteButtonClicked);
+
+
+
+
 function submitButtonClicked(){
     const inputValue = document.querySelector('.textField');
+    inputValue.placeholder = 'Введите новую задачу!'
     taskArrays.push({value:inputValue.value,id:taskArrays.length});
    
     renderTask(inputValue.value);
     inputValue.value = '';
+
+    const buttonDel = document.querySelectorAll('.deletButton');
+    const array = Array.from(buttonDel)
+    array.forEach((elem) => {
+        const id = array.indexOf(elem)
+        
+        console.log(array)
+        elem.addEventListener('click',deleteButtonClicked(id))
+    })
+    
+
 }
-function deleteButtonClicked(){
-    console.log('dd')
+function deleteButtonClicked(id){
+    taskArrays.forEach((task) => {
+       
+    })
 }
 function renderTask(textValue){
     const header = document.querySelector('.header');
@@ -21,47 +37,52 @@ function renderTask(textValue){
     
     
 
-
-    if (taskArrays.length===1){
-        const container =  document.createElement('div');
-        container.classList.add('container')
-
-        const spanTask = document.createElement('span');
-        spanTask.classList.add('task');
-        spanTask.textContent = `${taskArrays.length}.`  + textValue;
-
-        const spanGarbage = document.createElement('span')
-        spanGarbage.classList.add('garbage')
-        spanGarbage.textContent = 'мусор'
-        spanGarbage.innerHTML = '<img class="icon" src="img/icon.png" alt="">'
-
-        
-        
-        header.after(tasks);
-        tasks.append(container);
-        container.append(spanTask)
-        container.append(spanGarbage)
-
+    if (textValue){
+        if (taskArrays.length===1){
+            const container =  document.createElement('div');
+            container.classList.add('container')
+    
+            const spanTask = document.createElement('span');
+            spanTask.classList.add('task');
+            spanTask.textContent = `${taskArrays.length}.`  + textValue;
+    
+            const spanGarbage = document.createElement('span')
+            spanGarbage.classList.add('deletButton')
+            spanGarbage.textContent = 'мусор'
+            spanGarbage.innerHTML = '<img class="icon" src="img/icon.png" alt="">'
+    
+            
+            
+            header.after(tasks);
+            tasks.append(container);
+            container.append(spanTask)
+            container.append(spanGarbage)
+    
+        }
+        else{
+            const container =  document.createElement('div');
+            container.classList.add('container')
+    
+            const spanTask = document.createElement('span');
+            spanTask.classList.add('task');
+            spanTask.textContent = `${taskArrays.length}.`  + textValue;
+            
+            const spanGarbage = document.createElement('span')
+            spanGarbage.classList.add('deletButton')
+            spanGarbage.textContent = 'мусор'
+            spanGarbage.innerHTML = '<img class="icon" src="img/icon.png" alt="">'
+    
+            tasks.append(container);
+            container.append(spanTask)
+            container.append(spanGarbage)
+        }
+       
     }
     else{
-
-        const container =  document.createElement('div');
-        container.classList.add('container')
-
-        const spanTask = document.createElement('span');
-        spanTask.classList.add('task');
-        spanTask.textContent = `${taskArrays.length}.`  + textValue;
-        
-        const spanGarbage = document.createElement('span')
-        spanGarbage.classList.add('garbage')
-        spanGarbage.textContent = 'мусор'
-        spanGarbage.innerHTML = '<img class="icon" src="img/icon.png" alt="">'
-
-        tasks.append(container);
-        container.append(spanTask)
-        container.append(spanGarbage)
+        const inputValue = document.querySelector('.textField');
+        inputValue.placeholder = 'Пустая строка, введите задачу!'
     }
-   console.log(taskArrays);
-}
+    }
+    
 
 
