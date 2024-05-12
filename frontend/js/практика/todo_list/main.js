2feac190a9ff14322ee881127972cd1baed53126
ZhaminZhalilov <146634615+ZhaminZhalilov@users.noreply.github.com>
2024-05-12 11:41:40 +0300

    final

--- frontend/js/практика/todo_list/main.js
+++ frontend/js/практика/todo_list/main.js
@@ -1,4 +1,5 @@
 let  taskArrays = [];
+let delButton = []
 
 const button = document.querySelector('.buttonAdd');
 button.addEventListener('click',submitButtonClicked);
@@ -10,26 +11,35 @@ button.addEventListener('click',submitButtonClicked);
 function submitButtonClicked(){
     const inputValue = document.querySelector('.textField');
     inputValue.placeholder = 'Введите новую задачу!'
-    taskArrays.push({value:inputValue.value,id:taskArrays.length});
-   
-    renderTask(inputValue.value);
+    if (inputValue.value !==''){
+        taskArrays.push({value:inputValue.value,id:taskArrays.length})
+    }
+    
+    
+    renderTask(inputValue);
     inputValue.value = '';
 
     const buttonDel = document.querySelectorAll('.deletButton');
     const array = Array.from(buttonDel)
-    array.forEach((elem) => {
-        const id = array.indexOf(elem)
+    for (let i = 0; array.length > i;i++){
+        const button = array[i];
+        button.id = i;
+        button.addEventListener('click',deleteButtonClicked)
+      
         
-        console.log(array)
-        elem.addEventListener('click',deleteButtonClicked(id))
-    })
+    }
     
 
 }
-function deleteButtonClicked(id){
-    taskArrays.forEach((task) => {
-       
-    })
+function deleteButtonClicked(){
+    const delId = parseFloat(event.target.id);
+    const buttonDell = document.getElementById(`${delId}`)
+    const container = buttonDell.parentNode
+    container.remove()
+    
+    
+    
+    
 }
 function renderTask(textValue){
     const header = document.querySelector('.header');
@@ -37,14 +47,14 @@ function renderTask(textValue){
     
     
 
-    if (textValue){
+    if (textValue.value){
         if (taskArrays.length===1){
             const container =  document.createElement('div');
             container.classList.add('container')
     
             const spanTask = document.createElement('span');
             spanTask.classList.add('task');
-            spanTask.textContent = `${taskArrays.length}.`  + textValue;
+            spanTask.textContent =  textValue.value;
     
             const spanGarbage = document.createElement('span')
             spanGarbage.classList.add('deletButton')
@@ -57,6 +67,8 @@ function renderTask(textValue){
             tasks.append(container);
             container.append(spanTask)
             container.append(spanGarbage)
+            delButton.push(textValue)
+            
     
         }
         else{
@@ -65,7 +77,7 @@ function renderTask(textValue){
     
             const spanTask = document.createElement('span');
             spanTask.classList.add('task');
-            spanTask.textContent = `${taskArrays.length}.`  + textValue;
+            spanTask.textContent = textValue.value;
             
             const spanGarbage = document.createElement('span')
             spanGarbage.classList.add('deletButton')
@@ -75,7 +87,10 @@ function renderTask(textValue){
             tasks.append(container);
             container.append(spanTask)
             container.append(spanGarbage)
+            delButton.push(textValue)
+            console.log(delButton)
         }
+
        
     }
     else{
